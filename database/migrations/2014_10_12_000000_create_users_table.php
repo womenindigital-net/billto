d37de31730b64aa210713590b47acd0058b8bdb0
Md.yousuf <mdy506071@gmail.com>
2022-11-16 18:09:46 +0600

    user profile design

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
+            $table->string('address')->nullable();
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('profileImage')->nullable();
             $table->boolean('is_admin')->default(false);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
