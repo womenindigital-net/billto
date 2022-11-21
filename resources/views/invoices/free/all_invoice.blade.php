95044d20815c928e88595551af68246cd7e0f521
Kuhelika-Puja <88089199+Kuhelika-Puja@users.noreply.github.com>
2022-11-21 15:17:52 +0600

    invoice_6 added

--- resources/views/invoices/free/all_invoice.blade.php
+++ resources/views/invoices/free/all_invoice.blade.php
@@ -1,7 +1,7 @@
  @if("1"==$invoiceData->template_name)
- @include('invoices.free.invoice_4')
+ @include('invoices.free.invoice_5')
  @elseif ("3"==$invoiceData->template_name)
- @include('invoices.free.invoice_4')
+ @include('invoices.free.invoice_5')
  @endif
 
 
